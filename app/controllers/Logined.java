9600686eaa1d6fadcf17b5e58d29dc32caad7890
xiusheng chen <xiusonchan@gmail.com>
2014-02-19 19:25:15 +0800

    modified some files

--- app/controllers/Logined.java
+++ app/controllers/Logined.java
@@ -34,9 +34,7 @@ public class Logined extends Controller {
             //跳转到登录画面
             Application.index();
         }
-
-List<Post> postList = Post.find("order by id desc").from(0).fetch(5);
-    	
+List<Post> postList = Post.find("order by id desc").from(0).fetch(5);	
        render(postList);
     }
     
@@ -61,62 +59,22 @@ List<Post> postList = Post.find("order by id desc").from(0).fetch(5);
         email("发送成功！");
     }
     
-    public static void order(String message){
-    	render(message);
-    }
-    
     public static void order_cms(){
     	if(connected() == null) {
             //跳转到登录画面
             Application.index();
         }
-		
-    	//找到已经送货的订单
-    	List<Ordered> orderedSendedList = Ordered.find("bySended",true).fetch();
-    	//找到还没送货的订单
-    	List<Ordered> orderedNoSendedList = Ordered.find("bySended",false).fetch();
-       render(orderedSendedList,orderedNoSendedList);
+       render();
     }
 
-    public static void order_refresh(long itemId){
-    	if(connected() == null) {
-            //跳转到登录画面
-            Application.index();
-        }
-		
-    	//找到对应数据项
-    	Ordered ordered = Ordered.findById(itemId);
-    	ordered.sended = true;
-    	ordered.messages = "555";
-    	ordered.cupCount = 123;
-    	ordered.save();
-    	//order_cms();
-    	Map map = new HashMap();
-    	int status = 200;
-    	map.put("status", status);
-    	renderJSON(map);
-		
-    }    
-    
-    public static void order_dele(long itemId){
-    	if(connected() == null) {
-            //跳转到登录画面
-            Application.index();
-        }
-		
-    	//找到对应数据项
-    	int status = 0;
-    	Ordered ordered = Ordered.findById(itemId);
-    	if(ordered != null){
-    		ordered.delete();
-        	status = 200;
-    	}
-    	Map map = new HashMap();    	
-   		map.put("status", status);
-    	renderJSON(map); 	
-		
-    }
-    
+public static void savePost(Post post) {
+
+}
+
+
+/**
+  *在首页保存留言的方法
+  */
     public static void savePost( Post post) {
 		if(connected()==null)
 		{
@@ -133,22 +91,16 @@ List<Post> postList = Post.find("order by id desc").from(0).fetch(5);
 	
         post.author = author;  
 		post.postedAt= new Date();
-                
-        // Validate
-        //validation.valid(post);
-        //if(validation.hasErrors()) {
-        //   render("@form", post);
-        //}
-
         // Save
         post.save();     
 		flash.success("Thanks for posting %s", author);
 		words_board();
-		
     }
 
+/**
+  *在“更多留言”保存留言的方法
+  */
 	public static void savePost2( Post post) {
-
 		if(connected()==null)
 		{
             //跳转到登录画面
@@ -164,21 +116,16 @@ List<Post> postList = Post.find("order by id desc").from(0).fetch(5);
 	
         post.author = author;  
 		post.postedAt= new Date();
-                
-        // Validate
-        //validation.valid(post);
-        //if(validation.hasErrors()) {
-        //   render("@form", post);
-        //}
-
         // Save
         post.save();     
 		flash.success("Thanks for posting %s", author);
 		index();
     }
 
+/**
+  *管理员页面保存留言的方法
+  */
 	 public static void savePostForCMS( Post post) {
-
 		if(connected()==null)
 		{
             //跳转到登录画面
@@ -194,13 +141,6 @@ List<Post> postList = Post.find("order by id desc").from(0).fetch(5);
 	
         post.author = author;  
 		post.postedAt= new Date();
-                
-        // Validate
-        //validation.valid(post);
-        //if(validation.hasErrors()) {
-        //   render("@form", post);
-        //}
-
         // Save
         post.save();     
 		flash.success("Thanks for posting %s", author);
@@ -209,25 +149,16 @@ List<Post> postList = Post.find("order by id desc").from(0).fetch(5);
     }
 
 	public static void words_board(){
-    
     	//从form+1开始返回后面的fetch条记录
     	List<Post> postList = Post.find("order by id desc").fetch();
     	
        render(postList);
     }
 
-	public static void words_board2(){
-    
-    	//从form+1开始返回后面的fetch条记录
-    	List<Post> postList = Post.find("order by id desc").from(0).fetch(10);
-    	
-       render(postList);
-    }
-
-	 public static void postComment(
-        Long postId, String content
-		 )
-         
+/**
+  *在首页的留言评论
+  */
+	 public static void postComment(Long postId, String content )
     {
 		if(connected()==null)
 		{
@@ -242,10 +173,10 @@ List<Post> postList = Post.find("order by id desc").from(0).fetch(5);
         words_board();
     }
 
-	 public static void postComment2(
-        Long postId, String content
-		 )
-         
+/**
+  *在留言板的留言评论
+  */
+	 public static void postComment2(Long postId, String content)
     {
 		if(connected()==null)
 		{
@@ -260,11 +191,11 @@ List<Post> postList = Post.find("order by id desc").from(0).fetch(5);
         index();
     }
 
-	 public static void postCommentForCMS(
-        Long postId, String content
-		 )
-         
-    {
+/**
+  *管理员的留言评论
+  */
+	 public static void postCommentForCMS(Long postId, String content)
+     {
 		if(connected()==null)
 		{
             //跳转到登录画面
@@ -280,13 +211,10 @@ List<Post> postList = Post.find("order by id desc").from(0).fetch(5);
 
 	public static void post_cms( ) {
 		List<Post> postList = Post.find("order by id desc").from(0).fetch(10);
-    	
        render(postList);
-		//render();
 	}
 
-	 public static void deletePost( Long postId)
-         
+	 public static void deletePost( Long postId)       
     {
 		if(connected()==null)
 		{
@@ -313,38 +241,21 @@ List<Post> postList = Post.find("order by id desc").from(0).fetch(5);
         post_cms();
     }
 
-	public static void myOrder()
-	{	
-		User user = connected();
-		if(user == null)
-		{
-            //跳转到登录画面
-            Application.login();
-        }
-
-		List<Ordered> orderList = Ordered.find("byUser", user).fetch();		
-        render(orderList);
-	}
 	
 	 public static void order_customer(){
 		User user = connected();
     	if(user == null) {
             //跳转到登录画面
             Application.index();
-        }
-		
+        }		
        render();
     }
-
 		public static void changePassword(String newPassword, String verifyNewPassword) {
-
 		validation.required(verifyNewPassword);
         validation.equals(verifyNewPassword, newPassword).message("Your password doesn't match");
         if(validation.hasErrors()) {
             render("@Logined.order_customer", verifyNewPassword);
          }
-
 		  flash.success("修改成功" );
 		}
-
 }
