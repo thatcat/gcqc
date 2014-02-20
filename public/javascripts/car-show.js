34e1b2d48f949933d0d2042528e0b022b913d281
liugc <610368688@qq.com>
2014-02-20 15:03:46 +0800

    praise number from sql

--- public/javascripts/car-show.js
+++ public/javascripts/car-show.js
@@ -1,7 +1,11 @@
 jQuery(document).ready(function() {
 	var box = jQuery("#content-box");
+	var praise = [];
+	for (var i=0; i<40; i++) {
+		praise[i] = jQuery(".brandParise").eq(i).val();
+	}
 	for (var i=1; i<=40; i++) {
-		box.append("<div class='car-pic'><a href='#' class='car car" + i + "'></a><a href='#' id='praise_btn" + i + "' class='praise_btn'><cite class='praise_btn_inner'><u class='WB_ico_logo'></u><cite class='praise_text'>赞</cite></cite></a><span class='praise_num num" + i + "'>0</span></div>");
+		box.append("<div class='car-pic'><a href='#' class='car car" + i + "'></a><a href='#' id='praise_btn" + i + "' class='praise_btn'><cite class='praise_btn_inner'><u class='WB_ico_logo'></u><cite class='praise_text'>赞</cite></cite></a><span class='praise_num num" + i + "'>" + praise[i-1] + "</span></div>");
 	}
 	jQuery(".praise_btn").click(function() {
 		var praise_num = jQuery(this).parent().find(".praise_num");
