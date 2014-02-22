dbc57fdee87ec72533d69f5674a12ea6706cd68f
liugc <610368688@qq.com>
2014-02-22 22:15:24 +0800

    noye

--- public/javascripts/showSeries.js
+++ public/javascripts/showSeries.js
@@ -1,6 +1,20 @@
 $(document).ready(function() {
-    var level = $("#level").text();
-
+    var level = $("#level").val()
+    if (level == "1.0") {
+        $("#levelShow").append("<ul class='hasStar'><li>1</li></ul>");
+    }
+    if (level == "2.0") {
+        $("#levelShow").append("<ul class='hasStar'><li>1</li><li>2</li></ul>");
+    }
+    if (level == "3.0") {
+        $("#levelShow").append("<ul class='hasStar'><li>1</li><li>2</li><li>3</li></ul>");
+    }
+    if (level == "4.0") {
+        $("#levelShow").append("<ul class='hasStar'><li>1</li><li>2</li><li>3</li><li>4</li></ul>");
+    }
+    if (level == "5.0") {
+        $("#levelShow").append("<ul class='hasStar'><li>1</li><li>2</li><li>3</li><li>4</li><li>5</li></ul>");
+    }
 
     $("#commentBtn").ajax({
         type: "GET",
